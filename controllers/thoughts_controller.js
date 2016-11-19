fd1b27b1090120c5ae8d71b107d624c1bff06ec0
Maria Beane <mbeane@findyr.com>
2016-11-19 10:35:14 -0600

    fixed size

--- controllers/thoughts_controller.js
+++ controllers/thoughts_controller.js
@@ -22,9 +22,6 @@ router.get('/', function(req, res) {
             }
         }).then(function(thoughts) {
             res.render('thoughts/index', {
-                // user_id: req.session.user_id,
-                // email: req.session.user_email,
-                // logged_in: req.session.logged_in,
                 thoughts: thoughts
             });
         });
@@ -41,9 +38,6 @@ router.get('/list', function(req, res) {
             }
         }).then(function(thoughts) {
             res.render('thoughts/list', {
-                // user_id: req.session.user_id,
-                // email: req.session.user_email,
-                // logged_in: req.session.logged_in,
                 thoughts: thoughts
             });
         });
